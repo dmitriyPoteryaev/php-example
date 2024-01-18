709259ac0ea1735bdd3c6c0394360499fd98882c
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2024-01-18 16:57:25 +0300

    working with form

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Akytec</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<form action="index.php" method="post">
+    Имя:  <input type="text" name="personal[name]" /><br />
+    Email: <input type="text" name="personal[email]" /><br />
+    Пиво: <br />
+    <select multiple name="beer[]">
+        <option value="warthog">Warthog</option>
+        <option value="guinness">Guinness</option>
+        <option value="stuttgarter">Stuttgarter Schwabenbräu</option>
+    </select><br />
+    <input type="submit" value="Отправь меня!" />
+</form>
+
+</html>
+
 <?php
-    echo "Hello Habrahabr111!";
+
+if ($_POST) {
+
+    echo  htmlspecialchars("{$_POST["personal"]["name"]}");
+
+ 
+}
+
+
+// This is comment
 ?>
