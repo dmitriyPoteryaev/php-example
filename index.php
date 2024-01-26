115f1f73c60b38a61377ab9619ecbcc96d1902ab
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2024-01-26 15:36:44 +0300

    working with js

--- index.php
+++ index.php
@@ -2,6 +2,34 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
+    $files = scandir("/var/www/html/uploads");
+
+    function filterFilesArray($files)
+    {
+
+        $FIRST_PATH_TO_ANOTHER_DIR =  0;
+
+        $SECOND_PATH_TO_ANOTHER_DIR = 1;
+
+        $EMPTY_ARRAY = [];
+
+        $i = 0;
+
+        foreach ($files as $file) {
+
+            if ($i != $FIRST_PATH_TO_ANOTHER_DIR && $i != $SECOND_PATH_TO_ANOTHER_DIR) {
+
+                array_push($EMPTY_ARRAY, $file);
+            }
+
+            $i++;
+        }
+
+        return $EMPTY_ARRAY;
+    }
+
+    echo "<script> const  ArraySeverFiles = " . json_encode(filterFilesArray($files)). ";</script>";
+
 ?>
 
     <!DOCTYPE html>
@@ -13,13 +41,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     </head>
 
     <body>
-        <h2>Загрузка файла</h2>
-        <form method="post" enctype="multipart/form-data">
+        <h2>Загрузка файла или сохранение файла</h2>
+             <form method="post" enctype="multipart/form-data">
             Выберите файл: <input type="file" name="filename" /><br /><br />
             <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-            <input type="submit" value="Загрузить" />
-        </form>
+            <input id="upload_btn" name="upload" type="submit" value="Выгрузить с сервера" />
+            <input id="save_btn" name="save" type="submit" value="Сохранить на сервер" />
+            <?php
+        if(count(filterFilesArray($files)) == 0){
 
+        echo "На сервере не имеется файлов <br>";
+
+        } else{
+            echo ' <select name="files" id="files">';
+
+            foreach (filterFilesArray($files) as $file) {
+
+           echo '<option value="' . $file .'">' . $file . '</option>';
+
+            }
+          
+            echo  '</select>';
+
+        }
+
+         ?>
+        </form>
+        <script>
+            console.log(ArraySeverFiles);
+            let save_button = document.getElementById("upload_btn");
+
+            if (ArraySeverFiles.length === 0) {
+
+                save_button.disabled = true;
+            }
+        </script>
     </body>
 
     </html>
@@ -42,48 +98,65 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $MIN_FILE_SIZE = 10000;
 
-        $full_file_type = $_FILES['filename']['type'];
+        function uploadFile()
+        {
 
-        $specific_file_type = strstr($full_file_type, "/", true);
+    
+            $basedir = '/var/www/html/uploads/';
+         
+            if(isset($_POST["save"])){
 
-        function uploadFile() {
+                $name = $_FILES["filename"]["name"];
 
-            $full_file_type = $_FILES['filename']['type'];
+                $path_info = pathinfo("/var/www/html/uploads/" . $name);
     
-            $specific_file_type = strstr($full_file_type, "/", true);
+                $file_extension = $path_info['extension'];
     
-            if($specific_file_type != "text") {
+                if ($file_extension != "md") {
     
-                echo "Type file isn't text! Choose other file! <br/> ";
+                    echo "Type file $file_extension! Choose other file!  <br/>";
     
+                    return;
+                }
+    
+
+                $savefile = $basedir . basename($name);
+
+                move_uploaded_file($_FILES["filename"]["tmp_name"], $savefile);
+
                 return;
+
             }
-    
-            $name = $_FILES["filename"]["name"];
-    
-            move_uploaded_file($_FILES["filename"]["tmp_name"], $name);
-    
-            //  скачиваем файл с серера
-    
-            header("Content-Disposition: attachment; filename=$name");
-    
-            header('Content-Type: text/plain');
-    
-            readfile($name);
+
+            if(isset($_POST["upload"])){
+
+  //  скачиваем файл с серера
+
+             $selectedFiles = $_POST["files"];
+
+             $uploadfile = $basedir . $selectedFiles;
+
+             header("Content-Disposition: attachment; filename=$selectedFiles");
+
+             header('Content-Type: text/plain');
+             readfile($uploadfile);
+
+             return;
+
+            }
+
         }
 
 
-        switch ($size_file) {
-            case $size_file >= $MAX_FILE_SIZE:
-                echo "Size file more then 30000 byte! It's so big file! Choose other file! <br/> ";
-                break;
-            default:
+        // switch ($size_file) {
+        //     case $size_file >= $MAX_FILE_SIZE:
+        //         uploadFile();
+        //         echo "Size file more then 30000 byte! It's so big file! Choose other file! <br/> ";
+        //         break;
+        //     default:
                 uploadFile();
-        }
-
-
+        // }
     }
-
 }
 
 ?>
