4cd4e5a98a87e08308605272473111a621178ac3
Poteryaev <d.poteryaev@akytec.rs>
2024-09-09 18:40:03 +0200

    feature/ add mysql container

--- app/index.php
+++ app/index.php
@@ -1,10 +1,54 @@
 <?php
-// 111
 include "./utils/class.php";
+
 
 $state = new State;
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        if (isset($_POST['button1'])) {
+
+        $host = 'php-example_mysql_1';
+$db = 'my_database'; // Замените на имя вашей базы данных
+$user = 'user'; // Замените на ваше имя пользователя
+$pass = 'user_password'; // Замените на ваш пароль
+$charset = 'utf8mb4';
+
+// Создание DSN (Data Source Name)
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+// Опции для PDO
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    // Создание PDO объекта
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    
+    // SQL запрос на вставку данных
+    $sql = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+
+    // Подготовка запроса
+    $stmt = $pdo->prepare($sql);
+
+    // Присвоение значений
+    $username = 'new_user';
+    $email = 'user@example.com';
+    $password = password_hash('user_password', PASSWORD_DEFAULT); // Хеширование пароля
+
+    // Выполнение запроса с передачей параметров
+    $stmt->execute(['username' => $username, 'email' => $email, 'password' => $password]);
+
+     // После успешной вставки делаем редирект, чтобы избежать повторной отправки данных при перезагрузке
+
+    echo "Новый пользователь успешно добавлен!";
+} catch (PDOException $e) {
+    echo "Ошибка: " . $e->getMessage();
+}
+}
 
             $tmp_name = $_FILES["filename"]["tmp_name"];
 
@@ -34,6 +78,7 @@ $state = new State;
                     <label class="save">
                         <input type="file" name="filename" />
                         <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+                        <input type="submit" name="button1" value="Кнопка 1">
                         <button id="save_btn" name="save">Save on server</button>
                     </label>
                     <span class="error">
